734fd23902b71b317e12204e04a9d9619d6b0cce
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-08-02 00:52:13 +0300

    ders1 ders 5 kadar

---
+++ ders4.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "21"
+	var b = "21"
+	fmt.Println("a+b")
+
+}
---
+++ toplama.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	sayi1 := 21
+	sayi2 := 23
+	toplam := sayi1 + sayi2
+	fmt.Println("Toplam:", toplam)
+	var b = "21"
+	fmt.Println("b degeri:", b)
+
+}
