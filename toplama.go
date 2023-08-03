1c2fad1b28a1cd831cad8a6a50acb3f3be2f6e8a
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-08-04 02:42:23 +0300

    go
    go donügsü ve bir kac kısa bilgiler

--- toplama.go
+++ toplama.go
@@ -7,7 +7,19 @@ func main() {
 	sayi2 := 23
 	toplam := sayi1 + sayi2
 	fmt.Println("Toplam:", toplam)
-	var b = "21"
-	fmt.Println("b degeri:", b)
+
+	deger1 := 1
+	deger2 := 2
+
+	cikarma := deger1 - deger2
+	fmt.Println("cıkarma", cikarma)
+
+	if deger1 == deger2 {
+		fmt.Println("sa")
+	} else if deger1 != deger2 {
+		fmt.Println("esit degil")
+	} else {
+		fmt.Println("hata")
+	}
 
 }
