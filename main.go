59129b9365031eba1e177b92b9e5a8793775a9e8
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-08-17 16:41:31 +0300

    gümlük go tozu

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
-var degisken_2 = "deger 2" //burada var  komutu ile degisken_2 diye bir deger verebildik  ama sadece func main altında da degisken_3 diye bir deger verebiliyoruz
+var degisken_2 = "deger 2" //burada var  komutu ile degisken_2 diye bir deger verebildik  ama sadece func main altında da degisken_3 diye bir deger verebiliyoruz . Dikket edilmesi gereken := olasmı
+var degisken_1 = "deger 1"
 
 func main() {
 	degisken_3 := "deger 3"
+	degisken_1 = " deger 2"
 
-	println(degisken_2, degisken_3)
+	println(degisken_2, degisken_3, degisken_1)
 
 }
