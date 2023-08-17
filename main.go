a02f84e84cae71c0a817709da74c7e0c5188e764
DZXX21 <91760460+DZXX21@users.noreply.github.com>
2023-08-17 16:28:10 +0300

    .

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+var degisken_2 = "deger 2" //burada var  komutu ile degisken_2 diye bir deger verebildik  ama sadece func main altında da degisken_3 diye bir deger verebiliyoruz
+
+func main() {
+	degisken_3 := "deger 3"
+
+	println(degisken_2, degisken_3)
+
+}
